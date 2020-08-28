8e9007c9b68a3cef2317be308859f62d88422dd6
vvatervvater <professionalhunter@ukr.net>
2020-08-28 12:24:49 +0300

    1 commit

---
+++ metacritic games/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  metacritic games
+//
+//  Created by ТАБЛЕТКИ on 27.08.2020.
+//  Copyright © 2020 water. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
